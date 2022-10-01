8b41adc0aeab24e211193e2431cc1943e1053c20
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2022-10-01 19:33:26 +0300

    first Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      "outfit": ["Outfit", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      }
+    },
+  },
+  plugins: [],
+}
